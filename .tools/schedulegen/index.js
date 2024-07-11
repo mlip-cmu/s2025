447047d624ede7bb55d9f32ad4c15dd6ea31194a
Christian Kaestner <kaestner@cs.cmu.edu>
2024-07-11 14:00:20 -0400

    fix workflow w/ secrets

--- .tools/schedulegen/index.js
+++ .tools/schedulegen/index.js
@@ -1,63 +1,83 @@
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 
-(async function (){
+(async function () {
 
-const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json',
-    scopes: 'https://www.googleapis.com/auth/spreadsheets.readonly'
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: 'https://www.googleapis.com/auth/spreadsheets.readonly'
     });
 
-const client = await auth.getClient();
-google.options({auth: client});
+    const client = await auth.getClient();
+    google.options({ auth: client });
 
 
-const prefix = "| Date  | Topic | Reading | Assignment due |\n| -     | -     | -       | -              |"
 
-console.log(prefix)
+    const spreadsheetId = process.env.SPREADSHEET_ID;
+    if (!spreadsheetId)
+        throw new Error("SPREADSHEET_ID not set");
 
-sheets.spreadsheets.values.get({
-    spreadsheetId: '1KWtPmFbMtOzsI0AkTeGLzHhnCZy_Z5SRR-Ud2PovN2U',
-    range: 'A:J',
-}, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-        rows.map((row) => {if (row[0] !== 'Date' && row[0] != '' && row[0] != undefined) {
-            const date = row[0] || ""
-            const id = row[1]
-            let topic = row[2] || ""
-            let assignment = row[4] || ""
-            const slidesLink = row[8]
-            const chapters = row[9] || ""
-            const readings = row[10] || ""
-            const assignmentLink = row[11] || ""
-            let badges = ""
-            if (topic.includes("rec"))
-                badges += "![Lab](https://img.shields.io/badge/-lab-yellow.svg) "
-            if (topic.includes("midterm"))
-                badges += "![Midterm](https://img.shields.io/badge/-midterm-blue.svg) "
-            if (topic.includes("break"))
-                badges += "![Break](https://img.shields.io/badge/-break-red.svg) "
+    const prefix = "| Date  | Topic | [Book Chapter](https://mlip-cmu.github.io/book/) | Reading | Assignment due |\n| -     | -     | -     | -       | -              |"
+    console.log(prefix)
 
-            const chapterLinks = chapters?.split(',').map(chapter => {
-                return `([${chapter.trim()}](https://mlip-cmu.github.io/book/${chapter.trim().padStart(2,'0')}/))`;
-            }).join(',');
+    sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: 'A:Z',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+            const columnIds = { date: null, topic: null, assignmentDue: null, slidesLink: null, bookChapters: null, reading: null, assignmentLink: null, id: null };
+            rows[0].forEach((header, index) => { 
+                if (header === "Date") columnIds.date = index; 
+                else if (header === "Topic") columnIds.topic = index; 
+                else if (header === "Assignment due") columnIds.assignmentDue = index;
+                else if (header === "Slides link") columnIds.slidesLink = index;
+                else if (header === "Book chapters") columnIds.bookChapters = index;
+                else if (header === "Reading") columnIds.reading = index;
+                else if (header === "Assignment link") columnIds.assignmentLink = index;
+                else if (header === "Id") columnIds.id = index;
+            });
 
-            if (slidesLink != undefined && slidesLink != "")
-                topic = `[${topic}](${slidesLink})`
 
-            if (assignmentLink != undefined && assignmentLink != "")
-                assignment = `[${assignment}](${assignmentLink})`            
-    
-            console.log(`| ${date} | ${badges}${topic} (${chapterLinks}) | ${readings} | ${assignment} |`)
-            
-        }});
-    } else {
-        console.log('No data found.');
-    }
-    console.log("\n\n")
-});
+
+            rows.map((row) => {
+                if (row[0] !== 'Date' && row[0] != '' && row[0] != undefined) {
+                    const date = row[columnIds.date] || "";
+                    const id = row[columnIds.id];
+                    let topic = row[columnIds.topic] || "";
+                    let assignment = row[columnIds.assignmentDue] || "";
+                    const slidesLink = row[columnIds.slidesLink];
+                    const chapters = row[columnIds.bookChapters] || "";
+                    const readings = row[columnIds.reading] || "";
+                    const assignmentLink = row[columnIds.assignmentLink] || "";
+                    let badges = ""
+                    if (id.includes("rec"))
+                        badges += "![Lab](https://img.shields.io/badge/-lab-yellow.svg) "
+                    if (id.includes("midterm"))
+                        badges += "![Midterm](https://img.shields.io/badge/-midterm-blue.svg) "
+                    if (id.includes("break"))
+                        badges += "![Break](https://img.shields.io/badge/-break-red.svg) "
+
+                    const chapterLinks = chapters?.split(',').map(chapter => {
+                        return `[${chapter.trim()}](https://mlip-cmu.github.io/book/${chapter.trim().padStart(2, '0')}/)`;
+                    }).join(',');
+
+                    if (slidesLink != undefined && slidesLink != "")
+                        topic = `[${topic}](${slidesLink})`
+
+                    if (assignmentLink != undefined && assignmentLink != "")
+                        assignment = `[${assignment}](${assignmentLink})`
+
+                    console.log(`| ${date} | ${badges}${topic} | ${chapterLinks} | ${readings} | ${assignment} |`)
+
+                }
+            });
+        } else {
+            console.log('No data found.');
+        }
+        console.log("\n\n")
+    });
 
 
 })();
